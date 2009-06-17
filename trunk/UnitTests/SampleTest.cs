073d08b5fbe91eca548f284e463fa57070de3a99
kzu.net <kzu.net@f7a126a6-5adc-11de-992f-fdd7e25b9163>
2009-06-17 01:43:00 +0000

    first sample test :)

---
+++ trunk/UnitTests/SampleTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Lopaco.Tests
+{
+	[TestFixture]
+	public class SampleTest
+	{
+		[Test]
+		public void SampleFoo()
+		{
+			Console.WriteLine("Foo");
+		}
+	}
+}
